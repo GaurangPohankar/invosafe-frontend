757a7e843494af212ac93f720551c842459ab874
gaurang7821939 <gaurang@bitvillegaming.com>
2025-07-10 19:34:41 +0530

    Update: my-api added

--- src/app/manager/credits/page.tsx
+++ src/app/manager/credits/page.tsx
@@ -14,7 +14,7 @@ export default function CreditsPage() {
           Credits & Billing
         </h2>
         <button className="inline-flex items-center justify-center rounded-md bg-brand-500 px-6 py-2.5 text-center font-medium text-white hover:bg-brand-600">
-          Add Credits
+        +  Add Credits
         </button>
       </div>
 
