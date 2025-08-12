fd4e7136ededd3d1b851632a3dd9e32692b0c4f5
gaurang7821939 <gaurang@bitvillegaming.com>
2025-08-12 22:39:35 +0530

    Update: bug fixes

--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import React from "react";
-import { EcommerceMetrics } from "@/components/ecommerce/manager/EcommerceMetrics";
 import MonthlyTarget from "@/components/ecommerce/manager/MonthlyTarget";
 import MonthlySalesChart from "@/components/ecommerce/manager/MonthlySalesChart";
 import StatisticsChart from "@/components/ecommerce/manager/StatisticsChart";
@@ -13,9 +12,12 @@ export const metadata: Metadata = {
 export default function ManagerDashboard() {
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
-      <div className="col-span-12 space-y-6 xl:col-span-7">
-        <EcommerceMetrics />
 
+      <div className="col-span-12">
+        <StatisticsChart />
+      </div>
+
+      <div className="col-span-12 space-y-6 xl:col-span-7">
         <MonthlySalesChart />
       </div>
 
@@ -23,9 +25,7 @@ export default function ManagerDashboard() {
         <MonthlyTarget />
       </div>
 
-      <div className="col-span-12">
-        <StatisticsChart />
-      </div>
+
 
     </div>
   );
